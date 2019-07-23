cee5114d7e7bb0162cbab0334c56d7dc8d3b2501
janAntu <randrus@ucsd.edu>
2019-07-23 13:17:42 -0700

    push to Github

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Editor, EditorState} from 'draft-js';
 import './App.css';
 
 function App() {
+    const [editorState, setEditorState] = React.useState(
+        EditorState.createEmpty()
+    );
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <p>Editor:</p>
       </header>
+        <Editor
+            editorState={editorState}
+            onChange={editorState => setEditorState(editorState)}
+        />
     </div>
   );
 }
